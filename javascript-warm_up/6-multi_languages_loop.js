d73c4d27aaeb2f5e3ea594554e219d371f8ebc43
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-03 19:20:33 +0100

    Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints 3 lines: (like 1-multi_languages.js) but by using an array of stiring and a loop
+
+let string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < string.length; i++){
+    console.log(string[i]);
+}
