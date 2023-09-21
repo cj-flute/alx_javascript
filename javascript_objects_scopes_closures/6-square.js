d442a998fc8e4047945773d03d38f830583dcdd6
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-21 02:45:08 +0100

    square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@
 const  Sqaure  = require('./5-square.js');
 
 class Square extends Sqaure {
-    charprint(c='X') {
+    charPrint(c='X') {
         for (let i = 0; i < this.height; i++){
             for (let j = 0; j < this.width; j++) { 
                 process.stdout.write(c);
