bdc673bdd8cc352f38f3462e6e258ab2b027a220
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-11-06 13:39:37 +0100

    Lorem

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const url = process.argv[2];
 const parts = [];
 
-request.get(url, { encoding: 'utf-8'})
+request.get(url)
     .on('body', (body) => {
         parts.push(body);
     })
