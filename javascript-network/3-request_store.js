b1267426137dcd6712a2a5b5f871b343c9646509
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-11-06 15:26:57 +0100

    Lorem

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,18 +1,14 @@
+#!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
-const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Usage: node script_name.js URL FILE_PATH');
-} else {
-  request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      fs.writeFileSync(filePath, body, 'utf-8');
-      console.log(`Web content successfully stored in ${filePath}`);
-    } else {
-      console.error('Failed to fetch data from the URL:', error);
-    }
-  });
-}
+request.get(url)
+    .on('body', (body) => {
+        const body_response = JSON.stringify(body);
+        console.log(body_response);
+
+    })
+    .pipe(fs.createWriteStream('body_response.html'))
