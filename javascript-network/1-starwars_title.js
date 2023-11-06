bae4ad52a1f3d883a20301c3cc8bd11bf26c93ae
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-11-06 09:17:23 +0100

    Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -2,11 +2,15 @@
 
 const request = require('request');
 const id = process.argv[2];
+const parts = [];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, {encoding : 'utf-8'})
     .on('data', (data) => {
-        const title = JSON.parse(data);
+        parts.push(data);
+    })
+    .on('complete', () => { 
+        const title = JSON.parse(parts);
         console.log(title.title);
     })
